bacf3148ac1ff808d18abbaa6d9947a4fc5267c8
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-03-06 21:59:43 +0530

    started adding main component

--- frontend/src/components/Homepage/Main/Main.jsx
+++ frontend/src/components/Homepage/Main/Main.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
+    <>
+      <div>
+        <div>India's best tech learning company</div>
+        <div>Learn industry relevant skills with top veterans</div>
+        <div>
+          <h2>Book a live class, for Free!</h2>
+          <form>
+            <div>Your topic of interest</div>
+            <label>
+              Program 1
+              <input type={"radio"} name="program" />
+            </label>
+            <label>
+              Program 2
+              <input type={"radio"} name="program" />
+            </label>
+            <label>
+              <input type={"text"} placeholder="Full Name" />
+            </label>
+            <label>
+              <input type={"email"} placeholder="Email" />
+            </label>
+            <label>
+              <input type={"number"} placeholder="Phone No." />
+            </label>
+            <div>Limited seats left</div>
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
