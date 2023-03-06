831170a58c2738df42ebc3aad9111ee494331cbf
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-03-06 21:42:09 +0530

    added program component

--- frontend/src/components/Homepage/Programs/Programs.jsx
+++ frontend/src/components/Homepage/Programs/Programs.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React from 'react';
+
 
 export const Programs = () => {
   return (
-    <div>Programs</div>
-  )
+    <>
+      <div>
+        <div className='text-center text-3xl'>Programs we offer</div>
+        <div className="flex flex-col gap-10 text-center">
+          <div className="shadow-2xl m-5 border-solid border ">
+            <div>
+              <div>Program 1</div>
+              <div>Icon</div>
+            </div>
+            <div>Career Outcome</div>
+            <div>Lorem ipsum</div>
+            <div>What you'll learn?</div>
+            <div>Lorem ipsum</div>
+            <div>
+              <div>Learn more</div>
+              <div>
+                <a href="tel:+919000000000">Call</a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-2xl m-5 border-solid border ">
+            <div>
+              <div>Program 2</div>
+              <div>Icon</div>
+            </div>
+            <div>Career Outcome</div>
+            <div>Lorem ipsum</div>
+            <div>What you'll learn?</div>
+            <div>Lorem ipsum</div>
+            <div>
+              <div>Learn more</div>
+              <div>
+                <a href="tel:+919000000000">Call</a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-2xl m-5 border-solid border ">
+            <div>
+              <div>Program 3</div>
+              <div>Icon</div>
+            </div>
+            <div>Career Outcome</div>
+            <div>Lorem ipsum</div>
+            <div>What you'll learn?</div>
+            <div>Lorem ipsum</div>
+            <div>
+              <div>Learn more</div>
+              <div>
+                <a href="tel:+919000000000">Call</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
